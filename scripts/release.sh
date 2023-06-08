22448b9d0a26af9c0768a92dbd725d922e988857
Chris Banes <chrisbanes@users.noreply.github.com>
2023-06-08 09:49:19 +0100

    Tweak release workflow

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,27 +9,34 @@ function getProperty() {
 }
 
 NEW_VERSION=$1
-SNAPSHOT_VERSION=$(getProperty 'VERSION_NAME' gradle.properties)
+NEW_SNAPSHOT_VERSION=$2
+CUR_SNAPSHOT_VERSION=$(getProperty 'VERSION_NAME' gradle.properties)
+
+if [ -z "$NEW_SNAPSHOT_VERSION" ]; then
+  # If no snapshot version was provided, use the current value
+  NEW_SNAPSHOT_VERSION=$CUR_SNAPSHOT_VERSION
+fi
 
 echo "Publishing $NEW_VERSION"
 
-SEDOPTION=
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  SEDOPTION="-i ''"
-fi
-
 # Prepare release
-sed $SEDOPTION "s/${SNAPSHOT_VERSION}/${NEW_VERSION}/g" gradle.properties
-git commit -am "Prepare for release v$NEW_VERSION"
-git tag "v$NEW_VERSION"
+sed -i.bak "s/${CUR_SNAPSHOT_VERSION}/${NEW_VERSION}/g" gradle.properties
+git add gradle.properties
+git commit -m "Prepare for release $NEW_VERSION"
 
 # Publish
 ./gradlew publish
 
+# Add git tag
+git tag "v$NEW_VERSION"
 # Prepare next snapshot
-echo "Restoring snapshot version $SNAPSHOT_VERSION"
-sed -i '' "s/${NEW_VERSION}/${SNAPSHOT_VERSION}/g" gradle.properties
-git commit -am "Prepare next development version"
+echo "Setting next snapshot version $NEW_SNAPSHOT_VERSION"
+sed -i.bak "s/${NEW_VERSION}/${NEW_SNAPSHOT_VERSION}/g" gradle.properties
+git add gradle.properties
+git commit -m "Prepare next development version"
+
+# Remove the backup file from sed edits
+rm gradle.properties.bak
 
 # Push it all up
 git push && git push --tags
